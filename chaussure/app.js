4db92a4ef67419c6c2eb0817bc7c333cbb810d8c
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-02-17 23:13:06 +0000

    a new commit

--- chaussure/app.js
+++ chaussure/app.js
@@ -16,6 +16,11 @@ const products = [
         img: "./img/air2.png",
       },
     ],
+    gallery: [
+      { img: "img/airNike.jpg", title: "Be Yourself!" },
+      { img: "img/airNike1.jpg", title: "This is the First Day of Your New Life" },
+      { img: "img/airNike3.jpg", title: "Just Do it!" }
+    ]
   },
   {
     id: 2,
@@ -30,6 +35,11 @@ const products = [
         code: "green",
         img: "./img/jordan2.png",
       },
+    ],
+    gallery: [
+      { img: "img/imgJordan.jpg", title: "Fly High!" },
+      { img: "img/imgJordan2.jpg", title: "Feel the Air" },
+      { img: "img/imgJordan3.jpg", title: "Legendary Moves" }
     ],
   },
   {
@@ -46,6 +56,11 @@ const products = [
         img: "./img/blazer2.png",
       },
     ],
+    gallery: [
+      { img: "./img/imgBlazer.jpg", title: "Step into Confidence" },
+      { img: "./img/imgBlazer2.jpg", title: "Classic Never Fades" },
+      { img: "./img/imgBlazer3.jpg", title: "Elegance in Motion" }
+    ],
   },
   {
     id: 4,
@@ -60,6 +75,11 @@ const products = [
         code: "lightgray",
         img: "./img/crater2.png",
       },
+    ],
+    gallery: [
+      { img: "./img/imgCrater.jpg", title: "Adventure Starts Here" },
+      { img: "./img/imgCrater2.jpeg", title: "Push Your Limits" },
+      { img: "./img/imgCrater3.jpg", title: "Bold and Unstoppable" }
     ],
   },
   {
@@ -76,8 +96,14 @@ const products = [
         img: "./img/hippie2.png",
       },
     ],
+    gallery: [
+      { img: "./img/hippie.jpg", title: "Freedom in Every Step" },
+      { img: "./img/hippie1.jpg", title: "Live the Moment" },
+      { img: "./img/hippie3.jpg", title: "Unleash Your Spirit" }
+    ],
   },
-];
+]; 
+
 
 let choosenProduct = products[0];
 
@@ -89,21 +115,20 @@ const currentProductSizes = document.querySelectorAll(".size");
 
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
-    //change the current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
-
-    //change the choosen product
     choosenProduct = products[index];
 
-    //change texts of currentProduct
     currentProductTitle.textContent = choosenProduct.title;
     currentProductPrice.textContent = "$" + choosenProduct.price;
     currentProductImg.src = choosenProduct.colors[0].img;
 
-    //assing new colors
     currentProductColors.forEach((color, index) => {
       color.style.backgroundColor = choosenProduct.colors[index].code;
     });
+
+    choosenProduct = products[index];
+
+    updateGallery(choosenProduct);
   });
 });
 
@@ -135,3 +160,36 @@ productButton.addEventListener("click", () => {
 close.addEventListener("click", () => {
   payment.style.display = "none";
 });
+
+const galleryContainer = document.querySelector(".gallery");
+
+// Fonction pour générer la galerie
+function updateGallery(product) {
+  galleryContainer.innerHTML = ""; 
+
+  product.gallery.forEach((item) => {
+    const galleryItem = document.createElement("div");
+    galleryItem.classList.add("galleryItem");
+
+    const img = document.createElement("img");
+    img.src = item.img;
+    img.classList.add("galleryImg");
+
+    const title = document.createElement("h1");
+    title.textContent = item.title;
+    title.classList.add("galleryTitle");
+
+    // Alterner image et titre pour respecter le format demandé
+    if (product.gallery.indexOf(item) % 2 === 0) {
+      galleryItem.appendChild(title);
+      galleryItem.appendChild(img);
+    } else {
+      galleryItem.appendChild(img);
+      galleryItem.appendChild(title);
+    }
+
+    galleryContainer.appendChild(galleryItem);
+  });
+}
+
+
